f04026c6b4052ed4d11650613f1165f3e1b68dfd
Abdelrahman Reda Fadl <31633565+boodyds@users.noreply.github.com>
2018-12-13 23:05:40 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Sequence.h"
+#include "DNA.h"
+#include "RNA.h"
+#include "Protein.h"
+#include "CodonsTable.h"
+#include "GUI.h"
+using namespace std;
+
+int main()
+{
+
+    GUI Application;
+    Application.start();
+
+}
